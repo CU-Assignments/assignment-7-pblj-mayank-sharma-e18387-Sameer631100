192acd164e3e623693e36f9b2490020eb95669c7
Sameer631100 <sameersardana926@gmail.com>
2025-04-23 21:44:29 +0530

    Create StudentAttendance.java

---
+++ medium/StudentAttendance.java
@@ -0,0 +1,14 @@
+public class StudentAttendance {
+    private int id;
+    private String name, date, status;
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
